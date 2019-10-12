3d6cf11f841e9f4b7c1ddb507b4f433b3b43f85c
shoken <syokenz@gmail.com>
2019-10-12 23:31:24 +0900

    initial commit

---
+++ ln_commands.sh
@@ -0,0 +1,6 @@
+ln -s ~/git/dotfiles/_zshrc ~/.zshrc
+ln -s ~/git/dotfiles/_zprofile ~/.zprofile
+ln -s ~/git/dotfiles/_gitconfig ~/.gitconfig
+ln -s ~/git/dotfiles/_irbrc ~/.irbrc
+ln -s ~/git/dotfiles/_gemrc ~/.gemrc
+ln -s ~/git/dotfiles/_config ~/.config
