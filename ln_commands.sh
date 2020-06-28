74875eaa002f2b33b3303596811d01a9b8f7ed59
shoken <syokenz@gmail.com>
2020-06-29 00:22:06 +0900

    update ln command

--- ln_commands.sh
+++ ln_commands.sh
@@ -3,4 +3,5 @@ ln -s ~/git/dotfiles/_zprofile ~/.zprofile
 ln -s ~/git/dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/git/dotfiles/_irbrc ~/.irbrc
 ln -s ~/git/dotfiles/_gemrc ~/.gemrc
+ln -s ~/git/dotfiles/_vimrc ~/.vimrc
 ln -s ~/git/dotfiles/_config ~/.config
